103612cd0f8471b41bb5846e238c5d7e6387a1ae
jullinator <justus.karlsson@hotmail.se>
2019-04-25 21:38:08 +0200

    Done with opcode encoding for mem_instructions.py

--- assembler/instructions/mem_instructions.py
+++ assembler/instructions/mem_instructions.py
@@ -2,20 +2,26 @@ from assembler.instructions.instructions import Instruction, fetch_registers, fe
 
 
 # load word
-Instruction("load", 0x11, chain(
+Instruction("load", 0b100000 , chain(
     fetch_size(),
     fetch_registers(2),
     fetch_immediate(3),
 ))
 
 
-Instruction("store", 0x12, chain(
+Instruction("store", 0b110100, chain(
     fetch_size(),
     fetch_registers(2),
     fetch_immediate(3),
 ))
 
-Instruction("mov", 0x4, chain(
+Instruction("store_pm", 0b110101, chain(
+    fetch_size(),
+    fetch_registers(2),
+    fetch_immediate(3),
+))
+
+Instruction("mov", 0b110011, chain(
     fetch_registers(2),
 ))
 
@@ -29,29 +35,23 @@ Instruction("movhi", 0x6, chain(
     fetch_immediate(2),
 ))
 
-# LEGACY
-Instruction("ldi", 0x5, chain(
-    fetch_registers(2),
-    fetch_immediate(3),
-))
-
-Instruction("push", 0x01, chain(
+Instruction("push", 0b110110, chain(
     fetch_size(),
     fetch_registers(1)
 ))
 
 
-Instruction("pop", 0x01, chain(
+Instruction("pop", 0b000010, chain(
     fetch_size(),
     fetch_registers(1)
 ))
 
 
-Instruction("in", 0x01, chain(
+Instruction("in", 0b000011, chain(
     fetch_immediate(1, length=5, ir_idx=6),
     fetch_registers(1, dest_start_index=11, args_idx=2)
 ))
-Instruction("out", 0x01, chain(
+Instruction("out", 0b000011, chain( # Didn't find any "OUT" encodingf so same as "IN"
     fetch_immediate(1, length=5, ir_idx=6),
     fetch_registers(1, dest_start_index=11, args_idx=2)
 ))
