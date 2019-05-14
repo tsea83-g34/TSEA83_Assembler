f84d33b04f5f497e519e6b716e61426ff760e9c9
Henrik Nilsson <henni142@student.liu.se>
2019-05-14 15:53:46 +0200

    Updated output files.

---
+++ out/program.vhd
@@ -0,0 +1,16 @@
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+use IEEE.NUMERIC_STD.ALL;
+
+library work;
+use work.PIPECPU_STD.ALL;
+
+package program_file is
+  
+  constant program : program_memory_array := (
+--$PROGRAM
+--$PROGRAM_END
+others => X"00000000"
+);
+
+end program_file;
