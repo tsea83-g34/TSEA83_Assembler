2ff7e1cdbf6e8a30fce462aa27ee3e0366124857
jullinator <justus.karlsson@hotmail.se>
2019-05-22 13:08:59 +0200

    Added out to repo

---
+++ out/program.vhd
@@ -0,0 +1,17 @@
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+use IEEE.NUMERIC_STD.ALL;
+
+library work;
+use work.PIPECPU_STD.ALL;
+
+package program_file is
+  
+  constant program : program_memory_array := (
+--$PROGRAM
+X"93110000", -- add r1 r1 r0
+--$PROGRAM_END
+others => X"00000000"
+);
+
+end program_file;
